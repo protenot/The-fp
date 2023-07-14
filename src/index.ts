9e384489f0e365e06dcdeb3d84b3d18600ee17b5
Olga_Belaya <protenot@yandex.ru>
2023-07-14 07:40:24 +0300

    change branch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { carry, func } from "./1_Carry";
+import { sumNumber, s, s3 } from "./2_sumNumber";
+import { Parallel } from "./3_Parallel";
+import { spiral } from "./4_spiral";
+import { semver } from "./5_semver";
+
 const hof: Function = carry(func);
 console.log(hof(1, 2, 3, 4, 5)); // 15
 console.log(hof(2, 3, 4)(5, 6)); // 20
@@ -6,7 +11,7 @@ console.log(hof(3, 4)(5, 6)(7)); // 25
 console.log(hof(4, 5)(6)(7, 8)); // 30
 console.log(hof(5)(6)(7)(8)(9)); // 35
 const div1 = document.createElement("div");
-document.append(div1);
+/* document.append(div1);
 const h1 = document.createElement("h1");
 div1.append(h1);
 h1.innerText = "Функция для каррирования";
@@ -25,8 +30,8 @@ p4.innerText = `hof(4, 5)(6)(7, 8): ${hof(4, 5)(6)(7, 8)}`;
 const p5 = document.createElement("p");
 div1.append(p5);
 p5.innerText = `hof(5)(6)(7)(8)(9): ${hof(5)(6)(7)(8)(9)}`;
-
-import { sumNumber, s, s3 } from "./2_sumNumber";
+ */
+//import { sumNumber, s, s3 } from "./2_sumNumber";
 alert(sumNumber()); // 0;
 
 alert(s(1)); // 1
@@ -36,7 +41,7 @@ alert(s(3)(4)(5)); // 12
 alert(s3(5)); // 8
 //alert(s3(6)); // 9
 
-import { Parallel } from "./3_Parallel";
+//import { Parallel } from "./3_Parallel";
 const runner = new Parallel(2);
 console.log(
   runner.jobs(
@@ -48,7 +53,7 @@ console.log(
   ),
 );
 
-import { spiral } from "./4_spiral";
+//import { spiral } from "./4_spiral";
 spiral([
   [0, 1, 2, 3, 4],
   [5, 6, 7, 8, 9],
@@ -57,7 +62,7 @@ spiral([
 ]);
 // [0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]
 const h2_3 = document.createElement("h2");
-div1.append(h2_3);
+/* div1.append(h2_3);
 h2_3.innerText = `Матрица, развернутая по спирали`;
 const div2 = document.createElement("div");
 div1.append(div2);
@@ -68,14 +73,14 @@ div2.innerText = `[0, 1, 2, 3, 4],\n  [5, 6, 7, 8, 9],\n [10, 11, 12, 13, 14],\n
     [10, 11, 12, 13, 14],
     [15, 16, 17, 18, 19],
   ],
-)}`;
-import { semver } from "./5_semver";
+)}`; */
+//import { semver } from "./5_semver";
 semver(["1.0.5", "2.5.0", "0.12.0", "1", "1.23.45", "1.4.50", "1.2.3.4.5.6.7"]); // [ "0.12.0", "1", "1.0.5", "1.2.3.4.5.6.7", "1.4.50", "1.23.45", "2.5.0" ]
 const h3_3 = document.createElement("h2");
-div1.append(h2_3);
+/* div1.append(h2_3);
 h3_3.innerText = `Сортировка semver`;
 const div3 = document.createElement("div");
 div1.append(div3);
 div3.innerText = `semver(["1.0.5", "2.5.0", "0.12.0", "1", "1.23.45", "1.4.50", "1.2.3.4.5.6.7"]): ${semver(
   ["1.0.5", "2.5.0", "0.12.0", "1", "1.23.45", "1.4.50", "1.2.3.4.5.6.7"],
-)}`;
+)}`; */
